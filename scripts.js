a1fd2fd4b94c09f42c6b259f5f0dfd27d8b642e7
Thiago Alves <thiagoalves060@gmail>
2020-06-27 08:06:48 -0300

    Criando modal

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const receitas = document.querySelectorAll('.card');
+
+for (let receita of receitas) {
+    receita.addEventListener("click", function() {
+        const imageId = receita.getAttribute('id');
+        const titulo = receita.querySelector('.titulo').innerText
+        const autor = receita.querySelector('.texto').innerText
+
+        modalOverlay.classList.add('active');
+        modalOverlay.querySelector('img').src = `assets/${imageId}.png`
+        modalOverlay.querySelector('h4').innerHTML = titulo;
+        modalOverlay.querySelector('p').innerHTML = autor;
+    })
+}
+
+document.querySelector('.close-modal').addEventListener("click", function() {
+    modalOverlay.classList.remove('active');
+})
